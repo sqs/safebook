49f05a266651ab44d4bf4248e5a5b56cfb2f0e0e
Quinn Slack <sqs@cs.stanford.edu>
2011-04-17 23:13:02 -0700

    avoid using local "user" in templates

--- profiles/views.py
+++ profiles/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     users = User.objects.all()
-    return render_to_response('profiles/index.html', {'users': users})
+    return render_to_response('profiles/index.html', {'allusers': users})
 
 def detail(request, username):
-    user = get_object_or_404(User, username=username)
-    return render_to_response('profiles/detail.html', {'user': user})
+    thisuser = get_object_or_404(User, username=username)
+    return render_to_response('profiles/detail.html', {'thisuser': thisuser})
 
