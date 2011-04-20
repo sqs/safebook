ae96aac377bbbb6b75b01f036dd23602e51bde68
Quinn Slack <sqs@cs.stanford.edu>
2011-04-19 20:48:20 -0700

    use RequestContext

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponseNotFound
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 def index(request):
     users = User.objects.all()
-    return render_to_response('profiles/index.html', {'allusers': users})
+    return render_to_response('profiles/index.html', {'allusers': users},
+                              context_instance=RequestContext(request))
 
 def detail(request, username):
     thisuser = get_object_or_404(User, username=username)
-    return render_to_response('profiles/detail.html', {'thisuser': thisuser})
+    return render_to_response('profiles/detail.html', {'thisuser': thisuser},
+                              context_instance=RequestContext(request))
 
