14a3844978172f2de43e0834c064d5b2e2033f42
Quinn Slack <sqs@cs.stanford.edu>
2011-05-02 17:45:55 -0700

    test base template logged in text

--- profiles/tests.py
+++ profiles/tests.py
@@ -19,3 +19,13 @@ class DetailTest(TestCase):
         res = self.client.get('/unknownuser')
         self.assertEquals(res.status_code, 404)
 
+class BaseTemplateTest(TestCase):
+    fixtures = ['test.json']
+
+    def test_show_logged_in_user(self):
+        res = self.client.get('/', SSL_SRP_USER='user')
+        self.assertContains(res, 'you are user')
+
+    def test_not_logged_in(self):
+        res = self.client.get('/')
+        self.assertContains(res, 'not logged in')
